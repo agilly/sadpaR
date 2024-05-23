dad39469455ad09ccc818992ff2673c27b723a3c
arthur gilly <arthur.gilly@protonmail.ch>
2024-05-22 21:03:15 -0400

    reverted to 4b901b4c636995c6adffa0e445fbc92f863d90b9

--- IntervalDetector/app.R
+++ IntervalDetector/app.R
@@ -750,12 +750,11 @@ output$CTInEditFrame=renderText({
     selectedRootDir=rootDir()
     if(length(selectedRootDir)){
       #withProgress(message = appLang$loadingDatasetModal, value = 0, {
-        ret=FALSE
      progressSweetAlert(session = session, id="loadDatasetPBar", value=0, display_pct=T, title="Loading dataset", status="warning", striped=T, size="l")
         if(checkSelectedFolder(session, input, output, rootDir, loadedDataset, currentTagging, dur, appPaths, appLang = appLang)){
-          ret=loadDataset(session, input, output, rootDir, loadedDataset, currentTagging, dur, savedRetag)
+          loadDataset(session, input, output, rootDir, loadedDataset, currentTagging, dur, savedRetag)
         }
-        if(ret) closeSweetAlert(session = session)
+        closeSweetAlert(session = session)
       #})
     }
   })
@@ -912,11 +911,10 @@ output$CTInEditFrame=renderText({
         # Merge the data.tables with adjusted image paths
         setnames(taggedEvents_dt, c("V2"), c("species"))
         #print(str(intervals_dt))
-        intervals_dt.copy=copy(intervals_dt)
-        intervals_dt.copy[, fn := sub(old_root_dir, "", fn, fixed = TRUE)]
-        intervals_dt.copy[,ctidint:=paste(ctid, interval)]
+        intervals_dt[, fn := sub(old_root_dir, "", fn, fixed = TRUE)]
+        intervals_dt[,ctidint:=paste(ctid, interval)]
         #intervals_dt[,c("ctid", "interval"):=NULL]
-        merged_dt = merge(taggedEvents_dt, intervals_dt.copy, by = 'ctidint')
+        merged_dt = merge(taggedEvents_dt, intervals_dt, by = 'ctidint')
         mergedDtForRetag(merged_dt)
       }
     }
